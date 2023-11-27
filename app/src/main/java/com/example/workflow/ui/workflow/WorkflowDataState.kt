d119ed25a9bacc99016c83f6b16fcbce07189d79
Cristipi <cristipigm@gmail.com>
2023-11-27 13:34:51 +0100

    add state and view model, set data into info screen

---
+++ app/src/main/java/com/example/workflow/ui/workflow/WorkflowDataState.kt
@@ -0,0 +1,7 @@
+package com.example.workflow.ui.workflow
+
+import com.example.workflow.domain.model.WorkflowModel
+
+data class WorkflowDataState (
+    val workflowInfo: WorkflowModel = WorkflowModel()
+)

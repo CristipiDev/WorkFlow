2b48eae029b07f324ac6925730b1cd6ad8613480
Cristipi <cristipigm@gmail.com>
2023-11-30 18:29:21 +0100

    Set an animate the button

--- app/src/main/java/com/example/workflow/ui/workflow/WorkflowScreen.kt
+++ app/src/main/java/com/example/workflow/ui/workflow/WorkflowScreen.kt
@@ -1,6 +1,8 @@
 package com.example.workflow.ui.workflow
 
-import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -13,8 +15,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -29,8 +33,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -53,19 +65,34 @@ fun WorkflowScreen(
         }
     }
 
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+    val density = LocalDensity.current
+    var iconHeight by remember { mutableStateOf(0.dp) }
+    var newTaskBoxHeight by remember { mutableStateOf(0.dp) }
+
+    var showNewTaskBox by remember { mutableStateOf(false) }
+    val rotateIcon by animateFloatAsState(
+        if (showNewTaskBox) -45f else 0f,
+        tween(durationMillis = 300), label = ""
+    )
+
+    val newTaksBoxOffset by animateDpAsState(
+        if (showNewTaskBox) newTaskBoxHeight+20.dp else 0.dp,
+        tween(durationMillis = 300), label = ""
+    )
+
     Box(modifier = Modifier
         .fillMaxSize()
         .background(MaterialTheme.colorScheme.primary)
         .padding(top = 40.dp)) {
 
-        Column(modifier = Modifier
+        Box(modifier = Modifier
             .fillMaxSize()
             .background(
                 MaterialTheme.colorScheme.background,
                 shape = RoundedCornerShape(20.dp, 20.dp, 0.dp, 0.dp)
-            )
-            .padding(20.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            )) {
+            Column(modifier = Modifier.padding(20.dp)) {
                 Row(verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier.verticalScroll(scrollState)) {
                     Text(text = viewModel.dataState.workflowInfo.workflowTitle,
@@ -81,18 +108,50 @@ fun WorkflowScreen(
                     }
                 }
             }
-            Row(modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.End) {
-                Box(modifier = Modifier
-                    .size(50.dp)
+            Column(modifier = Modifier.fillMaxWidth()
+                .offset(y = screenHeight-40.dp-iconHeight-20.dp-newTaksBoxOffset)) {
+                Row(modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .onGloballyPositioned {
+                        iconHeight = with(density) {
+                            it.size.height.toDp()
+                        }
+                    },
+                    horizontalArrangement = Arrangement.End) {
+                    Box(modifier = Modifier
+                        .size(50.dp, 40.dp)
+                        .background(
+                            MaterialTheme.colorScheme.primary,
+                            shape = RoundedCornerShape(20.dp, 20.dp, 0.dp, 0.dp))
+                        .clickable { showNewTaskBox = !showNewTaskBox }
+                        .padding(top = 5.dp),
+                        contentAlignment = Alignment.Center) {
+                        Icon( imageVector = Icons.Filled.Add,
+                            contentDescription = "",
+                            tint = MaterialTheme.colorScheme.background,
+                            modifier = Modifier
+                                .rotate(rotateIcon)
+                                .size(40.dp))
+                    }
+                }
+
+                Column(modifier = Modifier
                     .background(
                         MaterialTheme.colorScheme.primary,
-                        shape = CircleShape
-                    ),
-                    contentAlignment = Alignment.Center) {
-                    Icon( imageVector = Icons.Filled.Add,
-                        contentDescription = "",
-                        tint = MaterialTheme.colorScheme.background)
+                        RoundedCornerShape(20.dp, 20.dp, 0.dp, 0.dp)
+                    )
+                    .fillMaxWidth()
+                    .wrapContentHeight(Alignment.CenterVertically)
+                    .padding(20.dp)
+                    .onGloballyPositioned {
+                        newTaskBoxHeight = with(density) {
+                            it.size.height.toDp()
+                        }
+                    })
+                {
+                    Text(text = "New task",
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.background)
                 }
             }
 

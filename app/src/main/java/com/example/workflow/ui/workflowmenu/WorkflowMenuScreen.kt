a2e7ecae415d3662c13962e5e966658b16a6860e
Cristipi <cristipigm@gmail.com>
2023-12-06 12:44:05 +0100

    swipetodismiss option

--- app/src/main/java/com/example/workflow/ui/workflowmenu/WorkflowMenuScreen.kt
+++ app/src/main/java/com/example/workflow/ui/workflowmenu/WorkflowMenuScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,28 +20,36 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
-import com.example.workflow.ui.navigation.AppRoutes
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.example.workflow.ui.aboutapp.WorkflowAboutAppScreen
 import com.example.workflow.ui.common.CustomBasicTextFieldComponent
 import com.example.workflow.ui.common.SwitchAndIconComponent
 import com.example.workflow.ui.common.TextButtonComponent
+import com.example.workflow.ui.navigation.AppRoutes
 
 @Composable
 fun WorkflowMenuScreen(
@@ -78,7 +87,7 @@ fun WorkflowMenuScreen(
             Column(
                 modifier = Modifier
                     .weight(1f)) {
-                LazyColumn() {
+                LazyColumn(modifier = Modifier.fillMaxWidth()) {
                     items(viewModel.dataState.workflowList) { workflow ->
                         MenuItem(workflow.workflowId, workflow.workflowTitle, navController)
                     }
@@ -121,7 +130,8 @@ fun WorkflowMenuScreen(
 private fun HeaderRow(
     onClickAbout: () -> Unit
 ) {
-    Row(modifier = Modifier.fillMaxWidth()
+    Row(modifier = Modifier
+        .fillMaxWidth()
         .height(80.dp)
         .padding(horizontal = 10.dp)) {
         Text(text = "WorkFlow",
@@ -140,20 +150,54 @@ private fun HeaderRow(
     }
 }
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun MenuItem(
     id: Int,
     text: String,
     navController: NavController
 ){
-    Text(text = text,
-        style = MaterialTheme.typography.labelMedium,
-        color = MaterialTheme.colorScheme.background,
-        modifier = Modifier
-            .padding(30.dp, 20.dp, 30.dp, 0.dp)
-            .fillMaxWidth()
-            .clickable { navController.navigate(AppRoutes.WorkflowInfo.route + "/$id") })
+    val state = rememberDismissState(
+        confirmValueChange = {
+            if(it == DismissValue.DismissedToStart){}
+            true
+        }
+    )
+    SwipeToDismiss(state = state,
+        background = {
+                     val color = when(state.dismissDirection){
+                         DismissDirection.EndToStart -> Color.Magenta
+                         DismissDirection.StartToEnd -> Color.Red
+                         null -> Color.Transparent
+                     }
+            Row(modifier = Modifier
+                .fillMaxSize()
+                .background(color)
+                .padding(30.dp, 10.dp, 0.dp, 10.dp),
+                horizontalArrangement = Arrangement.End) {
+
+                Icon(imageVector = Icons.Outlined.Favorite, contentDescription = "",
+                    modifier = Modifier.size(50.dp))
+                Icon(imageVector = Icons.Outlined.Delete, contentDescription = "",
+                    modifier = Modifier.size(50.dp))
+
+
+            }
+        },
+        dismissContent = {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary)) {
+                Text(text = text,
+                    style = MaterialTheme.typography.labelMedium,
+                    color = MaterialTheme.colorScheme.background,
+                    modifier = Modifier
+                        .padding(30.dp, 10.dp, 30.dp, 10.dp)
+                        .fillMaxWidth()
+                        .clickable { navController.navigate(AppRoutes.WorkflowInfo.route + "/$id") })
+            }
+        })
+
 }
 
 
@@ -190,10 +234,14 @@ fun NewWorkflowDialog(
                         tint = MaterialTheme.colorScheme.primary,
                         modifier = Modifier.clickable { onDismissRequest() }
                     )
-                    Spacer(modifier = Modifier.fillMaxWidth().height(10.dp))
+                    Spacer(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(10.dp))
                     CustomBasicTextFieldComponent(textFieldValue, onChangeTextField,
                         MaterialTheme.colorScheme.background, MaterialTheme.colorScheme.primary)
-                    Spacer(modifier = Modifier.fillMaxWidth().height(10.dp))
+                    Spacer(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(10.dp))
                     TextButtonComponent("Save", backgroundColor, buttonMainColor, {})
                 }
 
